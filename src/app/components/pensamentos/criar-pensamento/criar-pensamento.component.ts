9d04b468011ccd89e9ae4c84e38084f0c85315f5
Igor Henrique Salvador <igorsalvador0621@gmail.com>
2023-05-31 17:59:22 -0300

    Starting with angular framework and typescript
    Starting Memoteca project based on Angular Framework to study about it and Typescript to after apply on .NET.

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit{
+
+  pensamento = {
+    id : '1',
+    conteudo: 'Aprendendo Angular',
+    autoria : 'Dev',
+    modelo : 'modelo2'
+  }
+
+  constructor(){ }
+
+  ngOnInit(): void {
+
+  }
+
+  criarPensamento() {
+    window.alert("Novo pensamento criado!")
+  }
+
+  cancelar(){
+    window.alert("Ação cancelada!")
+  }
+}
+
+
